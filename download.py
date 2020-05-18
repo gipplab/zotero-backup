305c962e38eff20fd6aff74adad4573a0532e293
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-05-18 15:56:58 +0200

    Fix syntax error

--- download.py
+++ download.py
@@ -43,7 +43,7 @@ def get_bib_from_zotero(min_version=0, offset=0):
         "sort": "date",
         "tag": os.getenv("ZB_SEARCH_TAG"),
         "format": "json",
-        "include": "data,bib"
+        "include": "data,bib",
         "start": offset,
         "limit": 100
     }
