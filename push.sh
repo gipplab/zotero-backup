468ed843335ee35669f048f8d5adc7b9d5ca53ec
Moritz Schubotz <wiki@physikerwelt.de>
2020-05-18 15:22:16 +0200

    Rebase before pushing

--- push.sh
+++ push.sh
@@ -10,12 +10,14 @@ commit_files() {
   git add -A
   git commit -am "[skip ci] Update backup data (build $TRAVIS_BUILD_NUMBER)"
   git checkout -b updater
-  git branch -u origin/master     
+  git branch -u origin/master
+  git rebase
   cd ..
   git add -A
   git commit -am "[skip ci] Update link to backup data (build $TRAVIS_BUILD_NUMBER)"
   git checkout -b updater
-  git branch -u origin/master  
+  git branch -u origin/master
+  git rebase
   git push --recurse-submodules=on-demand origin updater:master
 }
 
