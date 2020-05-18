33738d4c441e3d94e96ad4eaa8f1d85f1df39d7a
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-05-18 08:28:07 +0200

    Update backup data (build )

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git add -A
+  git commit -am "Update backup data (build $TRAVIS_BUILD_NUMBER)"
+  git push --recurse-submodules=on-demand
+}
+
+
+setup_git
+commit_files
