c2c87b03a849c5d27f178095bb655c476a3db40d
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-05-18 14:35:03 +0200

    Update link to backup data (build )

--- push.sh
+++ push.sh
@@ -9,12 +9,14 @@ commit_files() {
   cd data
   git add -A
   git commit -am "Update backup data (build $TRAVIS_BUILD_NUMBER)"
-  git checkout -b updater origin/master
+  git checkout -b updater
+  git branch -u origin/master     
   cd ..
   git add -A
   git commit -am "Update link to backup data (build $TRAVIS_BUILD_NUMBER)"
-  git checkout -b updater origin/master
-  git push --recurse-submodules=on-demand
+  git checkout -b updater
+  git branch -u origin/master  
+  git push --recurse-submodules=on-demand origin updater:master
 }
 
 
