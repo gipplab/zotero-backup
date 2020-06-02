b2139c92560d2b23545901f764a65a66e70b5943
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-06-02 15:25:16 +0200

    Simplify push as two step approach

--- push.sh
+++ push.sh
@@ -12,6 +12,7 @@ commit_files() {
   git checkout -b updater
   git branch -u origin/master
   git rebase
+  git push
   cd ..
   git add -A
   git commit -am "[skip ci] Update link to backup data (build $TRAVIS_BUILD_NUMBER)"
@@ -20,7 +21,8 @@ commit_files() {
   git fetch ssh
   git branch -u ssh/master
   git rebase
-  git push --recurse-submodules=on-demand ssh updater:master
+  export GIT_SSH_COMMAND="ssh -i github_deploy_key_2"
+  git push ssh updater:master
 }
 
 
