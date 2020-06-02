714784a1b3cbda58ccf3a6c74212c7404b135b0d
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-06-02 15:54:43 +0200

    Fix relative location of deploy key 1

--- push.sh
+++ push.sh
@@ -9,7 +9,7 @@ commit_files() {
   cd data
   git add -A
   git commit -am "[skip ci] Update backup data (build $TRAVIS_BUILD_NUMBER)"
-  export GIT_SSH_COMMAND="ssh -i github_deploy_key"
+  export GIT_SSH_COMMAND="ssh -i ../github_deploy_key"
   git push origin HEAD:master
   cd ..
   git remote add ssh git@github.com:ag-gipp/zotero-backup.git
