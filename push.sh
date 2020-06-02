3d6c447d08229ef7bcf07ef06306df8da682e003
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-06-02 16:13:08 +0200

    USe absolute path to deploy keys

--- push.sh
+++ push.sh
@@ -9,14 +9,14 @@ commit_files() {
   cd data
   git add -A
   git commit -am "[skip ci] Update backup data (build $TRAVIS_BUILD_NUMBER)"
-  export GIT_SSH_COMMAND="ssh -i ../github_deploy_key"
+  export GIT_SSH_COMMAND="ssh -i $TRAVIS_BUILD_DIR/github_deploy_key"
   git push origin HEAD:master
   cd ..
   git remote add ssh git@github.com:ag-gipp/zotero-backup.git
   git fetch ssh
   git add -A
   git commit -am "[skip ci] Update link to backup data (build $TRAVIS_BUILD_NUMBER)"
-  export GIT_SSH_COMMAND="ssh -i github_deploy_key_2"
+  export GIT_SSH_COMMAND="ssh -i $TRAVIS_BUILD_DIR/github_deploy_key_2"
   git push ssh HEAD:master
 }
 
