34e5291921772bc21b069597e997f8b31918d4cb
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-06-25 15:24:18 +0200

    Add option to filter json output for specific keywords

--- extract-bibtex.py
+++ extract-bibtex.py
@@ -2,9 +2,24 @@ import json
 import sys
 
 filename = sys.argv[1]
+keyword = False
+try:
+    keyword = sys.argv[2]
+except IndexError:
+    pass
+
 with open(filename) as f:
     bibtex = json.loads(f.read())
     for entry in bibtex:
         biblatex = entry['biblatex']
         if len(biblatex) > 3:
-            print(biblatex)
+            if keyword:
+                tag_found = False
+                for tag in entry['data']['tags']:
+                    if tag['tag'] == keyword:
+                        tag_found = True
+                        break
+            else:
+                tag_found = True
+            if tag_found:
+                print(biblatex)
