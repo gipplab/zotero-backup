a2527f9b6b2f99475f26c8ab8d9ce0617db2d044
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-07-08 20:01:24 +0200

    Undo better bibtex hacks

--- extract-bibtex.py
+++ extract-bibtex.py
@@ -8,6 +8,17 @@ try:
 except IndexError:
     pass
 
+
+def undo_better_bibtex_hack(biblatex, extra=""):
+    biblatex = biblatex[:-2]
+    for line in extra.split("\n"):
+        if line.startswith("tex."):
+            payload = line[4:]
+            key, value = payload.split(": ", 1)
+            biblatex += f"\n\t{key} = {{{value}}},"
+    return biblatex+"\n}"
+
+
 with open(filename) as f:
     bibtex = json.loads(f.read())
     for entry in bibtex:
@@ -22,4 +33,7 @@ with open(filename) as f:
             else:
                 tag_found = True
             if tag_found:
+                extra_ = entry['data']['extra']
+                if extra_:
+                    biblatex = undo_better_bibtex_hack(biblatex, extra_)
                 print(biblatex)
